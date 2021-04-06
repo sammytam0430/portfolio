1e779e9c5ae3f4b151dbcac19e495ba6b2e31c98
sammy <sammytam2005@hotmail.com>
2021-04-05 22:14:47 -0700

    added jumbotron template

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <div class="heading p-5 bg-dark text-light">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+        <p class="col-md-8 fs-4">
+          Using a series of utilities, you can create this jumbotron, just like
+          the one in previous versions of Bootstrap. Check out the examples
+          below for how you can remix and restyle it to your liking.
+        </p>
+        <button class="btn btn-primary btn-lg" type="button">
+          Example button
+        </button>
+      </div>
+    </div>
+    <div class="summary"></div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.heading {
+  background-image: url('../assets/about.jpeg');
+  background-size: fixed;
+  background-position: center;
+  height: 93vh;
+}
+</style>
