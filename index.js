33f443174644507d4409cbc4c6d8257f51d56218
sammy <sammytam2005@hotmail.com>
2021-04-20 23:58:04 -0700

    added serve Static to serve front end files

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 const port = 5000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use('/', serveStatic(path.join('client/dist')));
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
