4b44103351c770d219cf42efbdb3872063605142
sammy <sammytam2005@hotmail.com>
2021-04-21 01:00:23 -0700

    changed port to env or 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const cors = require('cors');
 const path = require('path');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
