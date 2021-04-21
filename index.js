3a349ec6c83225c5037cc07e4953ce639d0d8825
sammy <sammytam2005@hotmail.com>
2021-04-21 01:40:52 -0700

    added connect history api fallback

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-//app.use(require('connect-history-api-fallback')());
+app.use(require('connect-history-api-fallback')());
 
 app.use('/', serveStatic(path.join(__dirname + '/client/dist')));
 
