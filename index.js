cc599cb64a02db63e6b52426b797d539d79b53bd
sammy <sammytam2005@hotmail.com>
2021-04-21 00:44:27 -0700

    changed path

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const cors = require('cors');
 const path = require('path');
 const port = 5000;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+//app.use(require('connect-history-api-fallback')());
 
-app.use('/', serveStatic(path.join('client/dist')));
+app.use('/', serveStatic(path.join(__dirname + '/client/dist')));
 
 app.listen(port, () => {
   console.log(`Listening at port ${port}`);
