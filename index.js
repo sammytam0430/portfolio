0f382b90eb56a5c2a7ad76074dae2c8393422f28
sammy <sammytam2005@hotmail.com>
2021-05-04 03:58:14 -0700

    adde api router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const port = 5000;
 const app = express();
+const api = require('./routes/api');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(require('connect-history-api-fallback')());
 
 app.use('/', serveStatic(path.join(__dirname + '/client/dist')));
+app.use('/api', api);
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Listening at port ${process.env.PORT || port}`);
